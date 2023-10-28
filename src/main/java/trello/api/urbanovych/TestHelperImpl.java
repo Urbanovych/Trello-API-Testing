6d34f1321fbb5c63906e42d552e0f58fc451061c
Urbanovych <urbanovych.qa@gmail.com>
2023-10-28 18:36:52 +0300

    YU: Added PropertiesHelper and properties for url and access token.

--- src/main/java/trello/api/urbanovych/TestHelperImpl.java
+++ src/main/java/trello/api/urbanovych/TestHelperImpl.java
@@ -4,11 +4,16 @@ import org.springframework.web.client.RestTemplate;
 import trello.api.urbanovych.endpoints.BoardsEndpoint;
 import trello.api.urbanovych.endpoints.ListsEndpoint;
 
+import java.util.Properties;
+
+import static trello.api.urbanovych.properties.PropertiesHelper.getPropertiesFromPath;
+
 public class TestHelperImpl implements TestHelper {
 
     private final RestTemplate restTemplate = new RestTemplate();
-    private final String key = "d6b36cb204310571665e1f549b0f8f75";
-    private final String token = "ATTAe2690e32af06d2406d6e5fc36234cfec754fc05a17a6c1250f18e62c57bd5bc97800BEE9";
+    Properties trelloAccess = getPropertiesFromPath("src/main/resources/trelloKeys.properties");
+    private final String key = trelloAccess.getProperty("key");
+    private final String token = trelloAccess.getProperty("token");
 
     // BOARDS
     public String getBoards() {
